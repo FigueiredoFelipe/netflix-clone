fd6f38f3afcaf131b18d0a1097db996b2f23dc3b
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2022-12-09 19:19:17 -0300

    feat: integration with api and basic syles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import MovieRow from './components/MovieRow/MovieRow';
 import Tmdb from './Tmdb';
 
+import './App.css';
+
 const App = () => {
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(() => {
     const loadAll = async () => {
       // Getting data
       let list = await Tmdb.getHomeList();
-      console.log(list);
-      // Tutorial paused at 41:12
+      setMovieList(list);
     };
     loadAll();
   }, []);
-  return <div>Hello world!</div>;
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 };
 
 export default App;
+// Aula parou no 55:49 https://www.youtube.com/watch?v=tBweoUiMsDg&list=WL&index=6&t=781s
